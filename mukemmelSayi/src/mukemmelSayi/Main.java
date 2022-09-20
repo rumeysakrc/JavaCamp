c5657006196af7da9354f48584e4719760676b41
Rumeysa <76470682+rumeysakrc@users.noreply.github.com>
2022-09-21 00:43:50 +0300

    odev2

---
+++ mukemmelSayi/src/mukemmelSayi/Main.java
@@ -0,0 +1,27 @@
+package mukemmelSayi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 496;
+		int total = 0;
+		
+		if (number<1) {
+			System.out.println("Geçersiz Sayi");
+			return;
+		}
+
+		for (int i = 1; i < number; i++) {
+			if (number % i == 0) {
+				total = total + i;
+			}
+		}
+		if (total == number) {
+			System.out.println("Mukemmel sayidir.");
+		}
+		else {
+			System.out.println("Mukemmel sayi degildir.");
+		}
+	}
+
+}
